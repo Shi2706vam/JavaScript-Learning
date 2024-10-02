585c4c6482a3273cec867a777a3a62db741c2128
Shi2706vam <87540830+Shi2706vam@users.noreply.github.com>
2024-10-02 15:54:16 +0000

    Conversion Of Datatypes

---
+++ 01-Basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+// let score = 33
+// let score = "33abc"
+// let score = null
+// let score = undefined
+// let score = "Shivam"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true +> 1; false => 0
+
+// let isLoggedIn = 1
+// let isLoggedIn = ""
+let isLoggedIn = "Shivam"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+// 0 => true; 1 => false
+// "" => flase
+// "Shivam" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
